6f003a8df9693b11315378e90008eeaac963647b
Jesús López-González <jesus@kathars.es>
2016-01-02 18:18:20 +0100

    Includes driver with `Coentity`'s final carrier

---
+++ src/main/scala/org/hablapps/coalgebraz/driver.scala
@@ -0,0 +1,32 @@
+package org.hablapps.coalgebraz
+
+import scalaz._, Scalaz._
+
+import Coalgebra._
+
+object Driver {
+
+  def runCoentity[I, O, B, X](
+      co: Coentity[I, O, B, X], x: X, in: List[I]): List[(List[O], B)] =
+    runHypertree(unfold(co, x), in)
+
+  def runHypertree[I, O, B](
+      ht: Hypertree[I, O, B], in: List[I]): List[(List[O], B)] = in match {
+    case (i::is) => {
+      val (os, ox) = ht.transition(i)
+      val v = List((os, ht.current))
+      ox.fold(v)(ht2 => v ++ runHypertree(ht2, is))
+    }
+    case Nil => List.empty
+  }
+
+  case class Hypertree[A, B, C](
+    current: C,
+    transition: A => (List[B], Option[Hypertree[A, B, C]]))
+
+  def unfold[I, O, B, X](
+      co: Coentity[I, O, B, X], x: X): Hypertree[I, O, B] = {
+    val Entity(obs, nxt) = co(x)
+    Hypertree(obs, i => nxt(i).map(_.map(unfold(co, _))))
+  }
+}
