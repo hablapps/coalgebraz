b9d274843f5d6e4fe239d38346cbbbcea8bf3340
Jesús López-González <jesus@kathars.es>
2016-01-15 11:33:05 +0100

    Provided `Read` instances for `BoardIn`

--- src/main/scala/org/hablapps/coalgebraz/Read.scala
+++ src/main/scala/org/hablapps/coalgebraz/Read.scala
@@ -5,5 +5,14 @@ trait Read[A] {
 }
 
 object Read {
+
   def apply[A](implicit R: Read[A]): Read[A] = R
+
+  implicit val readInt = new Read[Int] {
+    def read(s: String) = try {
+      Option(s.toInt)
+    } catch {
+      case _: NumberFormatException => None
+    }
+  }
 }
--- src/main/scala/org/hablapps/coalgebraz/candy/IO.scala
+++ src/main/scala/org/hablapps/coalgebraz/candy/IO.scala
@@ -2,6 +2,8 @@ package org.hablapps.coalgebraz.candy
 
 import scala.util.Random
 
+import scalaz._, Scalaz._
+
 import org.hablapps.coalgebraz._
 import Driver.runIO
 import Cocandy._
@@ -10,7 +12,33 @@ object IO extends App {
 
   def printGame(game: Game): Unit = ???
 
-  implicit val readBoardIn: Read[BoardIn] = ???
+  implicit val readDirection: Read[Direction] = new Read[Direction] {
+    def read(s: String) = s match {
+      case "up"    => Option(North)
+      case "down"  => Option(South)
+      case "left"  => Option(West)
+      case "right" => Option(East)
+      case _ => None
+    }
+  }
 
-  runIO(cogame(10), (Board(???, ???), new Random(), Nat(0)), printGame)
+  implicit val readBoardIn: Read[BoardIn] = new Read[BoardIn] {
+    def read(s: String) = {
+      s.split(" ") match {
+        case Array(a, b, c) => {
+          (Read[Int].read(a)
+           |@| Read[Int].read(b)
+           |@| Read[Direction].read(c)) { (x, y, dir) =>
+             Interchange((x, y), dir)
+           }
+        }
+        case _ => None
+      }
+    }
+  }
+
+  runIO(
+    cogame(10),
+    (Board(8, List.empty), new Random(), Nat(0)),
+    printGame)
 }
