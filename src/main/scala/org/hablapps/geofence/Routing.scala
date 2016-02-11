4f433ce5727a2c8075f7c17841830745314a7e5e
Jesús López-González <jesus@kathars.es>
2016-02-11 11:01:46 +0100

    `routeInGeofences` has been implemented

--- src/main/scala/org/hablapps/geofence/Routing.scala
+++ src/main/scala/org/hablapps/geofence/Routing.scala
@@ -2,10 +2,32 @@ package org.hablapps.geofence
 
 import scala.language.implicitConversions
 
+import scalaz._, Scalaz._
+
 import org.hablapps.coalgebraz._
 
 object Routing {
   implicit def routeInGeofences(
-    b: List[(String, Geofence)])(
-    i: IndexOut[GeoentityOut, Geoentity, String]): List[IndexIn[GeofenceIn, Geofence, String]] = ???
+    obs: List[(String, Geofence)])(
+    i: IndexOut[GeoentityOut, Geoentity, String]):
+      List[IndexIn[GeofenceIn, Geofence, String]] = i match {
+    case WrapOut((n, out)) => out match {
+      case Moved(pos) => {
+        def f(cnt: Boolean, cvr: Boolean, ev: String => GeofenceIn) = {
+          obs.filter { t =>
+            (t._2.elements contains n) == cnt && t._2.covers(pos) == cvr
+          }.map { t =>
+            WrapIn((t._1, ev(n))): IndexIn[GeofenceIn, Geofence, String]
+          }
+        }
+        f(true, false, Leave.apply) ++ f(false, true, Join.apply)
+      }
+      case Halted => {
+        obs.filter(_._2.elements contains n).map { t =>
+          WrapIn((t._1, Leave(n))): IndexIn[GeofenceIn, Geofence, String]
+        }
+      }
+    }
+    case _ => List.empty
+  }
 }
