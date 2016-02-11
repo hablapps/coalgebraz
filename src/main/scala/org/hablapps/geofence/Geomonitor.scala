a7f49d45e85f56a290fe3e4eb55abebc69c6a4ed
Jesús López-González <jesus@kathars.es>
2016-02-11 11:50:30 +0100

    Letting the type inference work

--- src/main/scala/org/hablapps/geofence/Geomonitor.scala
+++ src/main/scala/org/hablapps/geofence/Geomonitor.scala
@@ -23,26 +23,29 @@ object Geomonitor {
         (List(Left(id)), Option(x.copy(elements = x.elements - id)))
     })
 
-  val geoentities: Entity[
-      IndexIn[GeoentityIn, Geoentity, String],
-      IndexOut[GeoentityOut, Geoentity, String],
-      List[(String, Geoentity)],
-      List[Geoentity]] =
-    geoentity.index(_.id)
+  // Entity[
+  //   IndexIn[GeoentityIn, Geoentity, String],
+  //   IndexOut[GeoentityOut, Geoentity, String],
+  //   List[(String, Geoentity)],
+  //   List[Geoentity]]
+  val geoentities = geoentity.index(_.id)
 
-  val geofences: Entity[
-      IndexIn[GeofenceIn, Geofence, String],
-      IndexOut[GeofenceOut, Geofence, String],
-      List[(String, Geofence)],
-      List[Geofence]] =
-    geofence.index(_.id)
+  // Entity[
+  //   IndexIn[GeofenceIn, Geofence, String],
+  //   IndexOut[GeofenceOut, Geofence, String],
+  //   List[(String, Geofence)],
+  //   List[Geofence]]
+  val geofences = geofence.index(_.id)
 
-  val monitor: Entity[
-      IndexIn[GeoentityIn, Geoentity, String],
-      IndexOut[GeofenceOut, Geofence, String],
-      (List[(String, Geoentity)], List[(String, Geofence)]),
-      (List[Geoentity], List[Geofence])] =
-    geoentities |>| geofences.in[IndexOut[GeoentityOut, Geoentity, String]]
+  // FIXME: It's required to invoke `in` to force `geofences` to adapt its
+  // input. Can this be avoided?
+  //
+  // Entity[
+  //   IndexIn[GeoentityIn, Geoentity, String],
+  //   IndexOut[GeofenceOut, Geofence, String],
+  //   (List[(String, Geoentity)], List[(String, Geofence)]),
+  //   (List[Geoentity], List[Geofence])] =
+  val monitor = geoentities |>| geofences.in
 
   // val clock: Entity[Unit, ClockOut, Int, Int] =
   //   raw(identity, x => _ => (List(Tick(x + 1)), Option(x + 1)))
