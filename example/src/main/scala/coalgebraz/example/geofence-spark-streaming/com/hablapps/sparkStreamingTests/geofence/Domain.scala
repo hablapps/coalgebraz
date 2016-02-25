f2c04db16c6ff16a7faf14d49fa2df7dc27cc14b
Luis Rodero-Merino <luis.r.m@47deg.com>
2016-02-25 14:28:39 +0100

    Spark streaming geofence new watchers: stateless and stateless without side effects

--- example/src/main/scala/coalgebraz/example/geofence-spark-streaming/com/hablapps/sparkStreamingTests/geofence/Domain.scala
+++ example/src/main/scala/coalgebraz/example/geofence-spark-streaming/com/hablapps/sparkStreamingTests/geofence/Domain.scala
@@ -45,8 +45,63 @@ case class Watcher(val fences: List[Geofence]) {
       val out:List[(Geofence,Int)] = positions.getOrElse(e,Nil).filter{case (f:Geofence,t:Int) => !f.coordInFence(c)}
       in.foreach{case (f:Geofence,t:Int) => println(s"=> ${e.name} is entering '${f.name}'")}
       out.foreach{case (f:Geofence,t:Int) => println(s"=> ${e.name} is leaving '${f.name}' after '${tick-t}' ticks")}
-      positions.remove(e)
       positions.put(e, in)
     }    
   }
+}
+
+
+case object StatelessWatcher {
+  
+  // State is defined by present tick and map of entities <-> geofences,
+  // and it is passed in each call to process a new event.
+  case class State(tick: Int, positions: Map[Entity,List[(Geofence,Int)]])
+  
+  def processEvent(state: State, event: Event, fences: List[Geofence]): State = event match {
+    case Tick => {
+      println(s"TICKS: ${state.tick + 1}")
+      state.copy(tick = state.tick + 1)
+    }
+    case Halt(e: Entity) => {
+      val out:List[(Geofence,Int)] = state.positions.getOrElse(e, Nil)
+      out.foreach{case (f:Geofence,t:Int) => println(s"=> ${e.name} is leaving '${f.name}' after '${state.tick-t}' ticks")}
+      state.copy(positions = state.positions - e)
+    }
+    case Position(e: Entity, c: Coordinate) => {
+      val in:List[(Geofence,Int)] = fences.filter(_.coordInFence(c)).map((_,state.tick))
+      val out:List[(Geofence,Int)] = state.positions.getOrElse(e, Nil).filter{case (f:Geofence,t:Int) => !f.coordInFence(c)}
+      in.foreach{case (f:Geofence,t:Int) => println(s"=> ${e.name} is entering '${f.name}'")}
+      out.foreach{case (f:Geofence,t:Int) => println(s"=> ${e.name} is leaving '${f.name}' after '${state.tick-t}' ticks")}
+      state.copy(positions = state.positions + (e -> in))
+    }
+  }
+
+}
+
+
+case object StatelessWatcherSideEffects {
+  
+  // State is defined by present tick and map of entities <-> geofences,
+  // and it is passed in each call to process a new event.
+  case class State(tick: Int, positions: Map[Entity,List[(Geofence,Int)]], sideEffects: List[() => Unit])
+  
+  def processEvent(state: State, event: Event, fences: List[Geofence]): State = event match {
+    case Tick => {
+      val sideEffect = {() => println(s"TICKS: ${state.tick + 1}")}
+      state.copy(tick = state.tick + 1, sideEffects = state.sideEffects :+ sideEffect)
+    }
+    case Halt(e: Entity) => {
+      val out:List[(Geofence,Int)] = state.positions.getOrElse(e, Nil)
+      val newSideEffects = out.map{case (f:Geofence,t:Int) => {() => println(s"=> ${e.name} is leaving '${f.name}' after '${state.tick-t}' ticks")}}
+      state.copy(positions = state.positions - e, sideEffects = state.sideEffects ::: newSideEffects) 
+    }
+    case Position(e: Entity, c: Coordinate) => {
+      val in:List[(Geofence,Int)] = fences.filter(_.coordInFence(c)).map((_,state.tick))
+      val out:List[(Geofence,Int)] = state.positions.getOrElse(e,Nil).filter{case (f:Geofence,t:Int) => !f.coordInFence(c)}
+      val newSideEffects = in.map{case (f:Geofence,t:Int) => {() => println(s"=> ${e.name} is entering '${f.name}'")}} :::
+                           out.map{case (f:Geofence,t:Int) => {() => println(s"=> ${e.name} is leaving '${f.name}' after '${state.tick-t}' ticks")}}
+      state.copy(positions = state.positions + (e -> in), sideEffects = state.sideEffects ::: newSideEffects)
+    }
+  }
+
 }
