750d92508564563d9980cf639c52ac40c4d4f2e2
Jesús López-González <jesus@kathars.es>
2016-03-07 16:56:31 +0100

    Fundamental combinator `milner`

--- core/src/main/scala/coalgebraz/Milner.scala
+++ core/src/main/scala/coalgebraz/Milner.scala
@@ -3,4 +3,7 @@ package coalgebraz
 trait MilnerCore {
 
   type Milner[A, X] = Coalgebra[MilnerF[A, ?], X]
+
+  def milner[A, X](pi1: X => List[(A, X)]): Milner[A, X] =
+    pi1 andThen MilnerF.apply
 }
