92ff0ce92fe8537073456bce2c4a277ebf0b68a6
Luis Rodero-Merino <luis.rodero@hablapps.com>
2016-05-16 11:43:32 +0200

    Added return types to DStream transformations for better legilibility

--- example/src/main/scala/coalgebraz/example/geofence-spark-streaming/com/hablapps/sparkStreamingTests/IoTFence.scala
+++ example/src/main/scala/coalgebraz/example/geofence-spark-streaming/com/hablapps/sparkStreamingTests/IoTFence.scala
@@ -2,6 +2,7 @@ package com.hablapps.sparkStreamingTests
 
 import org.apache.spark._
 import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream.DStream
 
 import com.hablapps.sparkStreamingTests.geofence._
 
@@ -12,8 +13,8 @@ object IoTFence extends App {
   val batchInterval = Seconds(5) 
   val ssc = new StreamingContext(conf, batchInterval)
   
-  val lines = ssc.socketTextStream("localhost", 9999)
-  val events = lines.map(Event.parse).filter(_.isDefined).map(_.get) // parse
+  val lines: DStream[String] = ssc.socketTextStream("localhost", 9999)
+  val events: DStream[Event] = lines.map(Event.parse).filter(_.isDefined).map(_.get) // parse
   
   val fences = List(Geofence("a", Coordinate(1,1), 2, 3),
                     Geofence("b", Coordinate(1,1), 2, 3))
@@ -45,27 +46,27 @@ object IoTFence extends App {
   
   eventsPaired.updateStateByKey[State](updateState _)
               .foreachRDD(_.map(_._2).foreach(_ => ())) // To force processing
-  ssc.checkpoint(".")  // Mandatory when keeping state, so Spark knows where to save checkpoint it
+  ssc.checkpoint("./checkpoints")  // Mandatory when keeping state, so Spark knows where to save checkpoint it
   // -- STATE KEPT BY SPARK -- 
   */
                     
                     
   // -- STATE KEPT BY SPARK, TRANSFORMATION TO SIDE EFFECTS DSTREAM --
-  val eventsPaired = events.map(((),_)) // Spark keeps state by key. In our
-                                        // case state is global (i.e. unique)
-                                        // so there is only one key. We use
-                                        // Unit type and its only instance '()'
+  val eventsPaired: DStream[(Unit,Event)] = events.map(((),_)) // Spark keeps state by key. In our
+                                                              // case state is global (i.e. unique)
+                                                              // so there is only one key. We use
+                                                              // Unit type and its only instance '()'
   
   import StatelessWatcherSideEffects.State
-  val initialState = State(0, Map[Entity,List[(Geofence,Int)]](), List[() => Unit]())
+  val initialState: State = State(0, Map[Entity,List[(Geofence,Int)]](), List[() => Unit]())
   
   def updateState(events: Seq[Event], stateO: Option[State]): Option[State] = {
-    val presentState = stateO.getOrElse(initialState).copy(sideEffects = List[() => Unit]())
-    val newState = events.foldLeft[State](presentState)((s:State, e: Event) => StatelessWatcherSideEffects.processEvent(s, e, fences))
+    val presentState: State = stateO.getOrElse(initialState).copy(sideEffects = List[() => Unit]())
+    val newState: State = events.foldLeft[State](presentState)((s:State, e: Event) => StatelessWatcherSideEffects.processEvent(s, e, fences))
     Some(newState)
   }
   
-  val sideEffects = eventsPaired.updateStateByKey[State](updateState _).map(_._2.sideEffects.toList)  
+  val sideEffects: DStream[List[() => Unit]] = eventsPaired.updateStateByKey[State](updateState _).map(_._2.sideEffects.toList)  
   sideEffects.foreachRDD(_.foreach(l => l.foreach(sideEffect => sideEffect()))) // To force processing
   ssc.checkpoint("./checkpoints")  // Mandatory when keeping state, so Spark knows where to save checkpoint it
   // -- STATE KEPT BY SPARK, TRANSFORMATION TO SIDE EFFECTS DSTREAM --
